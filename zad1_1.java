7f2f5838cccc191eee07a0d13b85d7fca0d7b0cb
dawids12 <101203650+dawids12@users.noreply.github.com>
2022-03-22 09:47:41 +0000

    zad1_1

---
+++ zad1_1.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("H");
+    System.out.println("e");
+    System.out.println("l");
+    System.out.println("l");
+    System.out.println("o");
+    System.out.println(" ");
+    System.out.println("W");
+    System.out.println("o");
+    System.out.println("r");
+    System.out.println("l");
+    System.out.println("d");
+    }
+}
